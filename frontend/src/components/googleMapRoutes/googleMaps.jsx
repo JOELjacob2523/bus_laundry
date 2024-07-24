4a11fe7c4f2ea68364cdb7c849080e5cb64264bc
JOELjacob2523 <Shlomejacob@gmail.com>
2024-07-24 18:50:09 -0400

    ceated route list & able to download it

--- frontend/src/components/googleMapRoutes/googleMaps.jsx
+++ frontend/src/components/googleMapRoutes/googleMaps.jsx
@@ -7,7 +7,11 @@ import {
   useMap,
 } from "@vis.gl/react-google-maps";
 import API from "../GoogleAPIKey";
-import { Button, Card, Divider } from "antd";
+import { Button, Card, Divider, Select, Modal, message } from "antd";
+import { getAllUserInfo } from "../../servers/getRequest";
+import jsPDF from "jspdf";
+
+const { Option } = Select;
 
 function Directions() {
   const map = useMap();
@@ -16,8 +20,24 @@ function Directions() {
   const [directionsRenderer, setDirectionRenderer] = useState(null);
   const [routes, setRoutes] = useState([]);
   const [routesIndex, setRoutesIndex] = useState(0);
+  const [originInput, setOriginInput] = useState("");
+  const [destinationInput, setDestinationInput] = useState("");
+  const [validAddresses, setValidAddresses] = useState([]);
+  const [savedRoutes, setSavedRoutes] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const selected = routes[routesIndex];
   const leg = selected?.legs ? selected?.legs[0] : null;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const studentData = await getAllUserInfo();
+      const addresses = studentData
+        .map((student) => student.address2)
+        .filter((address) => address); // filter out empty addresses
+      setValidAddresses(addresses);
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (!routesLibary || !map) return;
@@ -26,9 +46,15 @@ function Directions() {
   }, [routesLibary, map]);
 
   useEffect(() => {
-    if (!directionsService || !directionsRenderer) return;
-    const origin = "7 Karlsburg Rd, Highland Mills, NY 10930";
-    const destination = "19 israel zupnick, monroe my 10950";
+    if (
+      !directionsService ||
+      !directionsRenderer ||
+      !originInput ||
+      !destinationInput
+    )
+      return;
+    const origin = originInput;
+    const destination = destinationInput;
     directionsService
       .route({
         origin: origin,
@@ -47,7 +73,7 @@ function Directions() {
       .catch((error) => {
         console.error("Error calculating directions:", error);
       });
-  }, [directionsService, directionsRenderer]);
+  }, [directionsService, directionsRenderer, originInput, destinationInput]);
 
   useEffect(() => {
     if (!directionsRenderer) return;
@@ -60,35 +86,167 @@ function Directions() {
     }
   }, [routesIndex, directionsRenderer]);
 
-  if (!leg) return null;
+  const handleOriginChange = (value) => {
+    setOriginInput(value);
+  };
+
+  const handleDestinationChange = (value) => {
+    setDestinationInput(value);
+  };
+
+  const handleSaveRoute = () => {
+    if (selected) {
+      setSavedRoutes((prevRoutes) => {
+        const updatedRoutes = [...prevRoutes, selected];
+        message.open({
+          type: "success",
+          content: "Route added successfully",
+        });
+        return updatedRoutes;
+      });
+    } else {
+      console.log("No route selected to save.");
+    }
+  };
+
+  useEffect(() => {
+    console.log("Updated Saved Routes:", savedRoutes);
+  }, [savedRoutes]);
+
+  const handleDownloadRoutes = () => {
+    const doc = new jsPDF();
+
+    savedRoutes.forEach((route, index) => {
+      doc.text(`Route ${index + 1}`, 10, 10 + index * 20);
+      doc.text(
+        `Start Address: ${route?.legs?.[0]?.start_address || "N/A"}`,
+        10,
+        20 + index * 20
+      );
+      doc.text(
+        `End Address: ${route?.legs?.[0]?.end_address || "N/A"}`,
+        10,
+        30 + index * 20
+      );
+      doc.text(`Summary: ${route.summary || "N/A"}`, 10, 40 + index * 20);
+    });
+
+    doc.save(`saved_routes_${new Date().toDateString()}.pdf`);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+    handleDownloadRoutes();
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
       <Card
-        title={selected.summary}
+        title={selected ? selected.summary : "Select origin and destination"}
         size="small"
         className="google_maps_directions_container"
       >
         <div>
           <div>
-            <strong>{leg.start_address?.split(",")[0]}</strong> to{" "}
-            <strong>{leg.end_address?.split(",")[0]}</strong>
+            <Select
+              showSearch
+              placeholder="Select origin"
+              value={originInput}
+              onChange={handleOriginChange}
+              style={{ width: 200, marginRight: 10 }}
+            >
+              {validAddresses.map((address, index) => (
+                <Option key={index} value={address}>
+                  {address}
+                </Option>
+              ))}
+            </Select>
+            <Select
+              showSearch
+              value={destinationInput}
+              placeholder="Select destination"
+              onChange={handleDestinationChange}
+              style={{ width: 200, marginRight: 10 }}
+            >
+              {validAddresses.map((address, index) => (
+                <Option key={index} value={address}>
+                  {address}
+                </Option>
+              ))}
+            </Select>
           </div>
-          <div>Distance: {leg.distance?.text}</div>
-          <div>Duration: {leg.duration?.text}</div>
+          <div>
+            {leg && (
+              <>
+                <div className="result_container">
+                  You selected:{" "}
+                  <strong>{leg.start_address?.split(",")[0]}</strong> to{" "}
+                  <strong>{leg.end_address?.split(",")[0]}</strong>
+                </div>
+              </>
+            )}
+          </div>
+          {leg && (
+            <>
+              <div className="distance_duration_container">
+                <div>Distance: {leg.distance?.text}</div>
+                <div>Duration: {leg.duration?.text}</div>
+              </div>
 
-          <Divider orientation="left">Other Routes</Divider>
-          <ul>
-            {routes.map((route, index) => (
-              <li key={route.summary}>
-                <Button type="text" onClick={() => setRoutesIndex(index)}>
-                  {route.summary}
+              <div className="save_route_btn_container">
+                <Button type="primary" onClick={handleSaveRoute}>
+                  Save Route
                 </Button>
-              </li>
-            ))}
-          </ul>
+              </div>
+
+              <Divider orientation="left">Other Routes</Divider>
+              <ul>
+                {routes.map((route, index) => (
+                  <li key={route.summary}>
+                    <Button type="text" onClick={() => setRoutesIndex(index)}>
+                      {route.summary}
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+              <div className="download_btn_container">
+                <Button
+                  type="default"
+                  onClick={showModal}
+                  style={{ marginTop: 10 }}
+                >
+                  Download Saved Routes
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </Card>
+      <Modal
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="Download"
+      >
+        <div>
+          {savedRoutes.map((route, index) => (
+            <div key={index}>
+              <div>
+                Start at: <strong>{route?.legs?.[0]?.start_address}</strong>
+              </div>{" "}
+              <div>
+                Going to: <strong>{route?.legs?.[0]?.end_address}</strong>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Modal>
     </div>
   );
 }
